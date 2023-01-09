da540bf78092209701e4eb7841c29b10cd7fe703
Sam Mix <1045262335@qq.com>
2023-01-09 05:26:49 +0000

    update somethings

--- constexpr-metaprogramming/GenPrimeTable.cpp
+++ constexpr-metaprogramming/GenPrimeTable.cpp
@@ -5,43 +5,41 @@
     > Mail: 1469709759@qq.com
     > Created Time: 2021-11-11 21:10
 ************************************************************************/
-#include <vector>
 #include <array>
+#include <cstdio>
+#include <vector>
 using namespace std;
 
 consteval vector<int> sievePrime(int n) {
-    vector<bool> marked(n + 1, true);
-    for (int p = 2; p * p <= n; p++) {
-        if (marked[p]) {
-            for (int i = p * p; i <= n; i += p)
-                marked[i] = false;
-        }
+  vector<bool> marked(n + 1, true);
+  for (int p = 2; p * p <= n; p++) {
+    if (marked[p]) {
+      for (int i = p * p; i <= n; i += p) marked[i] = false;
     }
+  }
 
-    vector<int> result;
-    for (int p = 2; p <= n; p++)
-        if (marked[p]) result.push_back(p);
-    return result;
+  vector<int> result;
+  for (int p = 2; p <= n; p++)
+    if (marked[p]) result.push_back(p);
+  return result;
 }
 
-consteval size_t primeCount(int n) {
-    return sievePrime(n).size();
-}
+consteval size_t primeCount(int n) { return sievePrime(n).size(); }
 static_assert(primeCount(100) == 25);
 
-template<int n>
+template <int n>
 consteval auto savePrimeToArray() {
-    array<int, primeCount(n)> result;
-    auto primes = sievePrime(n);
-    copy(primes.begin(), primes.end(), result.data());
-    return result;
+  array<int, primeCount(n)> result;
+  auto primes = sievePrime(n);
+  copy(primes.begin(), primes.end(), result.data());
+  return result;
 }
 
 constexpr auto primes100 = savePrimeToArray<10000>();
 
 int main() {
-    for (auto p: primes100) {
-        printf("%d\n", p);
-    }
-    return 0;
+  for (auto p : primes100) {
+    printf("%d\n", p);
+  }
+  return 0;
 }
